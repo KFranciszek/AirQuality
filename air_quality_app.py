ce284e300cb96cfd41de35aad3f10e04f05112e1
KFranciszek <kfzmarketing@gmail.com>
2023-04-04 10:19:38 +0200

    add chart,bugfix tabel data,preper to update button - air_quality_app.py

--- air_quality_app.py
+++ air_quality_app.py
@@ -3,15 +3,32 @@ import stations_map
 import streamlit as st
 from station_info import StationInfo
 from stations_map import StationsMap
+#from Initial_data_load_db import DataBaseWork
 import pandas
 import streamlit.components.v1 as components
 from unidecode import unidecode
+import  matplotlib as plt
 
 station_info = StationInfo()
 stations_map = StationsMap()
+#data_base_work = DataBaseWork()
 
 st.title("Air Quality App")
 st.sidebar.title("Data filtering")
+
+# style_tabel = """
+# <style>
+#     .reportview-container .main {
+#         max-width: 1500px;
+#         padding-top: 50px;
+#         padding-right: 100px;
+#         padding-left: 100px;
+#         padding-bottom: 50px;
+#     }
+# </style>
+# """
+
+
 
 def station_filtr(stations):
     station_list = [i[1] for i in stations]
@@ -27,12 +44,15 @@ def station_filtr(stations):
                 selected_parameter_index = parameters.index(selected_parameters)
                 parameters_id = sensors_mesure[selected_parameter_index][0]
                 parameters_data = station_info.sensors_data_by_sensors_db(parameters_id)
-                #df = pd.DataFrame(parameters_data)
                 df = pd.DataFrame(parameters_data, columns=['Kolumna 1', 'Kolumna 2', 'Kolumna 3','Kolumna 4'])
-                df = df.loc[:, ['Kolumna 3', 'Kolumna 4']].rename(
-                    columns={'Kolumna 3': 'Date', 'Kolumna 4': 'Value'})
+                df = df.loc[:, ['Kolumna 1', 'Kolumna 2']].rename(
+                    columns={'Kolumna 1': 'Date', 'Kolumna 2': 'Value'})
+                df = df.dropna(subset=['Value'])
                 st.write("Statistical data on the selected reagent.")
+                show_chart=st.line_chart(df,x="Date",y="Value")
                 st.write(df)
+
+
 
 #CITY CHOSE#
 city_name = st.sidebar.text_input("Find stations in a given city")
@@ -61,10 +81,11 @@ if city_map_show:
     # Read the content of the 'map.html' file
     with open("map.html", "r", encoding="utf-8") as file:
         map_html = file.read()
-
     # Display the HTML content of the map
     components.html(map_html, width=700, height=500)
 
 
-
+#DB UPDATE#
+#update_data = st.sidebar.button("Update data")
+#update_data = data_base_work.initial_payment_getData()
 
