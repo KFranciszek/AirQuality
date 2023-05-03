cc875734435a121761e74f686dd63c3fd01755ca
KFranciszek <kfzmarketing@gmail.com>
2023-05-03 19:58:45 +0200

    change of communication with the base to the context manager - "with"

--- Initial_data_load_db.py
+++ Initial_data_load_db.py
@@ -13,39 +13,17 @@ class DataBaseWork:
 
     """
 
-    def connect_db(self):
-        """
-
-        Establishing a connection to the database
-
-        """
-        db_name
-        conn = sqlite3.connect(db_name)
-        c = conn.cursor()
-        return conn, c
-
-    def execute_sql(self,conn,c,sql,values=None):
-
-        """
-
-        Executing a query on the database
-
-        """
-        if values:
-            c.execute(sql, values)
-        else:
-            c.execute(sql)
-
-
-    def db_operations(self):
-        db_name
-        with sqlite3.connect(db_name) as coon:
-            conn = sqlite3.connect(db_name)
-            if values:
-                c.execute(sql, values)
-            else:
-                c.execute(sql)
-
+    def db_operations(self, sql, values=None):
+        try:
+            with sqlite3.connect(db_name) as conn:
+                cursor = conn.cursor()
+                if values:
+                    cursor.execute(sql, values)
+                else:
+                    cursor.execute(sql)
+                psql = cursor.fetchall()
+        except (sqlite3.OperationalError, sqlite3.Error,) as e:
+            print("db error:", e)
 
 
 
@@ -54,43 +32,39 @@ class DataBaseWork:
         """Creation of 3 specific databases"""
 
         try:
-            db_name
-            #connect_db()
-            conn, c = self.connect_db()
+
             sql1 = '''CREATE TABLE "sensors" (
-                "sensor_id"	INT,
-                "stations_id"	INT,
-                "param_name"	VARCHAR(255),
-                "param_formula"	VARCHAR(20),
-                "param_code"	VARCHAR(20),
-                "param_id"	INT,
-                 UNIQUE(sensor_id,stations_id,param_name,param_formula,param_code,param_id))'''
+                   "sensor_id"	INT,
+                   "stations_id"	INT,
+                   "param_name"	VARCHAR(255),
+                   "param_formula"	VARCHAR(20),
+                   "param_code"	VARCHAR(20),
+                   "param_id"	INT,
+                    UNIQUE(sensor_id,stations_id,param_name,param_formula,param_code,param_id))'''
             sql2 = '''CREATE TABLE "sensors_data" (
-                "sensor_id"	INT,
-                "key"	VARCHAR(20),
-                "date"	datetime,
-                "value"	INT,
-                 UNIQUE(sensor_id, key, date, value))'''
+                   "sensor_id"	INT,
+                   "key"	VARCHAR(20),
+                   "date"	datetime,
+                   "value"	INT,
+                    UNIQUE(sensor_id, key, date, value))'''
             sql3 = '''CREATE TABLE "stations" (
-                "stations_id"	INT,
-                "station_name"	VARCHAR(255),
-                "gegr_lat"	VARCHAR(20),
-                "gegr_lon"	VARCHAR(20),
-                "city_id"	INT,
-                "city_name"	VARCHAR(255),
-                "commune_name"	VARCHAR(255),
-                "district_name"	VARCHAR(255),
-                "province_name"	VARCHAR(255),
-                "address_street" VARCHAR(255),
-                 UNIQUE(stations_id,station_name,gegr_lat,gegr_lon,city_id,city_name,
-                 commune_name,district_name,province_name,address_street))'''
+                   "stations_id"	INT,
+                   "station_name"	VARCHAR(255),
+                   "gegr_lat"	VARCHAR(20),
+                   "gegr_lon"	VARCHAR(20),
+                   "city_id"	INT,
+                   "city_name"	VARCHAR(255),
+                   "commune_name"	VARCHAR(255),
+                   "district_name"	VARCHAR(255),
+                   "province_name"	VARCHAR(255),
+                   "address_street" VARCHAR(255),
+                    UNIQUE(stations_id,station_name,gegr_lat,gegr_lon,city_id,city_name,
+                    commune_name,district_name,province_name,address_street))'''
             sql = [sql1,sql2,sql3]
             for i in sql:
-                self.execute_sql(conn, c, i)
-                conn.commit()
-        except (sqlite3.OperationalError,sqlite3.Error,) as e:
-            print("db error:",e)
-
+                self.db_operations(i)
+        except (sqlite3.OperationalError, sqlite3.Error,) as e:
+            print("db error:", e)
 
     def initial_payment_stations(self):
 
@@ -99,7 +73,6 @@ class DataBaseWork:
         api_url = 'https://api.gios.gov.pl/pjp-api/rest/station/findAll'
         response = api_connecting(api_url)
         data = response.json()
-        conn, c = self.connect_db()
         for station in data:
             city = station['city']
             commune = city['commune']
@@ -112,10 +85,7 @@ class DataBaseWork:
                       station['gegrLon'], city['id'], city['name'],
                       commune['communeName'], commune['districtName'],
                       commune['provinceName'], station['addressStreet'])
-            self.execute_sql(conn, c, sql, values)
-        conn.commit()
-        c.close()
-        conn.close()
+            self.db_operations(sql,values)
 
 
 
@@ -136,7 +106,6 @@ class DataBaseWork:
             response = api_connecting(api_url)
             data = response.json()
             sensors_data.append(data)
-            conn, c = self.connect_db()
             for sensor in data:
                 sql = '''INSERT OR IGNORE INTO sensors
                                   (sensor_id,stations_id, param_name, param_formula, param_code,
@@ -145,10 +114,8 @@ class DataBaseWork:
                 values =  (sensor['id'], sensor['stationId'], sensor['param']['paramName'],
                                sensor['param']['paramFormula'], sensor['param']['paramCode'], sensor['param']['idParam']
                                )
-                self.execute_sql(conn, c, sql, values)
-            conn.commit()
-            c.close()
-            conn.close()
+                self.db_operations(sql,values)
+
 
     def initial_payment_getData(self):
 
@@ -171,7 +138,6 @@ class DataBaseWork:
                     if value.get("value") is None:
                         sensors_data_null.append(dictionary)
                         sensors_data.remove(dictionary)
-        conn, c = self.connect_db()
         inserted_rows_count = 0
         for i in sensors_data:
             sensor_id = i['sensor_id']
@@ -186,9 +152,7 @@ class DataBaseWork:
                         sql = "INSERT OR IGNORE INTO sensors_data " \
                               "(sensor_id, key, date, value) VALUES (?, ?, ?, ?)"
                         values = (sensor_id, key, date, value)
-                        self.execute_sql(conn, c, sql, values)
-                        conn.commit()
+                        self.db_operations(sql,values)
                         inserted_rows_count += 1
-        c.close()
-        conn.close()
+
         print(f"Total rows inserted: {inserted_rows_count}")
